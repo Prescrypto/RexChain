6598caeb2df399cb5000100ec53996c98a29986b
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-07-07 16:05:28 -0500

    install postgres script

--- bin/setup_box.sh
+++ bin/setup_box.sh
@@ -5,6 +5,17 @@ sudo apt-get update
 sudo apt-get install -y build-essential libssl-dev wget python-pip python-dev libffi-dev
 sudo pip install -U pip
 
+# Install PostgreSQL
+echo "Installing postgresql"
+sudo apt-get install -y libpq-dev postgresql postgresql-contrib libxml2-dev libxslt1-dev zlib1g-dev build-essential libssl-dev libffi-dev
+# Create database if not exist
+psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'mydb'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE mydb"
+
+# Create user
+echo "Creating user and granting privileges"
+sudo -i -u postgres psql -c "CREATE USER vagrant WITH PASSWORD 'vagrant';"
+sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE mydb TO vagrant;"
+
 echo "=> Installing requirements..."
 sudo pip install -r /vagrant/requirements.txt
 
