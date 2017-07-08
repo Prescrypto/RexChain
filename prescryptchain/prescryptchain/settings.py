7f47a9042fe6f4106f9bc3dcc4199512c525eaf8
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-07-08 06:47:07 -0500

    add templates dir

--- prescryptchain/prescryptchain/settings.py
+++ prescryptchain/prescryptchain/settings.py
@@ -52,7 +52,7 @@ ROOT_URLCONF = 'prescryptchain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
