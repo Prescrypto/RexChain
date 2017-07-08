4fe0cb1c78dd88d46f0d10a725c14cd45e335678
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-07-08 05:40:17 -0500

    add pycrypto requirement

--- prescryptchain/prescryptchain/settings.py
+++ prescryptchain/prescryptchain/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = ast.literal_eval(os.environ['DEBUG_STATE'])
+PRODUCTION = ast.literal_eval(os.environ['PRODUCTION'])
 
 ALLOWED_HOSTS = ['*'] # temporary
 
