ccefdf360e9ab884a519b472aa29d73ca1fc3de0
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-07-08 17:37:09 -0500

    add api app

--- prescryptchain/blockchain/admin.py
+++ prescryptchain/blockchain/admin.py
@@ -4,6 +4,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Block, Prescription, Medication
 
+
+class PrescriptionAdmin(admin.ModelAdmin):
+    ''' Custom Prescription Admin  '''
+    search_fields = ['id']
+    list_per_page = 25
+    # inlines = [MedicationInline, ]
+
+
 # Register your models here.
 admin.site.register(Block)
 admin.site.register(Prescription)
