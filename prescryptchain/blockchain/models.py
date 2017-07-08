4da96c983e928d8ac50be08d4b7f088b19dd1a04
vash512 <phyrox.vash512@gmail.com>
2017-07-08 05:44:32 -0500

    cambios a los modelos

--- prescryptchain/blockchain/models.py
+++ prescryptchain/blockchain/models.py
@@ -13,12 +13,21 @@ from django.utils import timezone
 from core.utils import AESCipher
 # Create your models here.
 
+from django.utils.functional import cached_property
+from django.utils.dateformat import DateFormat
+
+
+
 @python_2_unicode_compatible
 class Block(models.Model):
     hash_n = models.CharField(max_length=255, default="")
     hash_m = models.CharField(max_length=255, default="")
     created_at = models.DateTimeField(default=timezone.now, db_index=True)
-    size =
+    @cached_property
+    def raw_size(self):
+        # get the size of the raw html
+        size = len(self.hash_n)+len(self.hash_m)+len(self.get_formatted_date())  * 8
+        return size
 
     # ======== Maybe this is useful????
     # # Hashes the request payload with utf-8 encoding
@@ -44,10 +53,17 @@ class Block(models.Model):
     #         AEScipher = AESCipher(sign.hash) # Accesing Signature property
     #         message = AEScipher.decrypt(message)
     #     return message
+    def get_formatted_date(self, format_time='d/m/Y'):
+        # Correct date and format
+        localised_date = self.created_at
+        if not settings.DEBUG:
+            localised_date = localised_date - timedelta(hours=6)
 
+        return DateFormat(localised_date).format(format_time)
 
     def __str__(self):
         return hash_m
+        
 
 # Simplified Rx Model
 @python_2_unicode_compatible
@@ -62,7 +78,6 @@ class Prescription(models.Model):
     timestamp = models.DateTimeField(default=datetime.now, db_index=True)
     location = models.CharField(blank=True, max_length=255, default="")
     raw_msg = models.TextField(max_length=10000, default="") # Anything can be stored here
-    location = models.CharField(max_length=255, default="")
     location_lat = models.FloatField(default=0) # For coordinates
     location_lon = models.FloatField(default=0)
     # Rx Specific
@@ -71,6 +86,9 @@ class Prescription(models.Model):
     bought = models.BooleanField(default=False)
     # Main
     signature = models.CharField(max_length=255, default="")
+
+
+    public_key = models.CharField(max_length=255, default="")
 
     # Hashes msg_html with utf-8 encoding, saves this in raw_html_msg and hash in signature
     def sign(self):
@@ -84,9 +102,36 @@ class Prescription(models.Model):
         self.raw_html_msg = msg.encode('utf-8')
         self.save()
 
+    def save(self):
+        self.encrypt()
+        super(Prescription, self).save()
+
+    def encrypt():
+        pass
+
+    def get_formatted_date(self, format_time='d/m/Y'):
+        # Correct date and format
+        localised_date = self.timestamp
+        if not settings.DEBUG:
+            localised_date = localised_date - timedelta(hours=6)
+
+        return DateFormat(localised_date).format(format_time)
+    @cached_property
+    def raw_size(self):
+        # get the size of the raw html
+        size = len(self.medic_public_key)+len(self.patient_public_key)+
+        len(self.diagnosis)+len(self.location)+
+        len(self.raw_msg)+len(self.location_lat)+
+        len(self.location_lon)+len(self.details)+
+        len(self.extras)+len(self.signature)+
+        len(self.public_key)+int(self.bought)+
+        len(self.get_formatted_date())  * 8 
+        return size
+
     def __str__(self):
         # podriamos reducirlo a solo nombre y poner los demas campos en el admin django! CHECAR  ESTO
         return signature
+
 
 @python_2_unicode_compatible
 class Medication(ValidateOnSaveMixin, models.Model):
@@ -102,5 +147,40 @@ class Medication(ValidateOnSaveMixin, models.Model):
     bought = models.BooleanField(default=False)
     drug_upc = models.CharField(blank=True, max_length=255, default="", db_index=True)
 
+    public_key = models.CharField(max_length=255, default="")
+        
+    def save(self):
+        self.encrypt()
+        super(Prescription, self).save()
+        
+    def encrypt():
+        pass
+
     def __str__(self):
         return drug_upc
+
+@python_2_unicode_compatible
+class Patient(models.Model):
+    """docstring for Patient"""
+    name = models.CharField(max_length=255)
+    email = models.EmailFild()
+    dob = models.DateTimeField(default=now())#fecha actual
+    public_key = models.CharField(max_length=255, default="")
+
+    def save(self):
+        self.encrypt()
+        super(Patient, self).self()
+        
+    def encrypt(self):
+        pass
+
+    def decrypt(self, key):
+        return ""
+
+    def __str__(self):
+        return drug_upc
+
+
+class BlockPrescription(models.Model):
+    block = models.ForeignKey("Block", blank=True, null=True)
+    prescription= models.ForeignKey("Prescription", unique= True)
