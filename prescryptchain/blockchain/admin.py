26e184d636e56b135d64b954e136a955a23c63fe
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-07-08 17:01:56 -0500

    add register admin sites for models

--- prescryptchain/blockchain/admin.py
+++ prescryptchain/blockchain/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Block, Prescription, Medication
 
 # Register your models here.
+admin.site.register(Block)
+admin.site.register(Prescription)
+admin.site.register(Medication)
