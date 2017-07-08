92135a005a6f9cb7047a85445aa8e24174799017
Everardo J. Barojas M <everardo@climb.mx>
2017-07-08 08:30:46 -0500

    Add first examples of asym crypto

---
+++ prescryptchain/blockchain/utils.py
@@ -0,0 +1,49 @@
+# -*- encoding: utf-8 -*-
+# AESCipher
+from core.utils import AESCipher
+## Hash lib
+import hashlib
+from datetime import timedelta, datetime
+# Unicode shite
+import unicodedata
+from django.utils.encoding import python_2_unicode_compatible
+import rsa
+
+# User A
+(AEncryptionPublicKey, AEncryptionPrivateKey) = rsa.newkeys(512)
+(ASigningPrivateKey, ASigningPublicKey) = rsa.newkeys(512)
+
+# User B
+(BEncryptionPublicKey, BEncryptionPrivateKey) = rsa.newkeys(512)
+(BSigningPrivateKey, BSigningPublicKey) = rsa.newkeys(512)
+
+# Encrypt
+cleartext="This is a secret text"
+encryptedtext=rsa.encrypt(cleartext, BEncryptionPublicKey)
+sentmessage=(encryptedtext)
+
+# Decrypt
+receivedmessage=sentmessage
+receivedencryptedtext=(receivedmessage)
+receivedcleartext=rsa.decrypt(receivedencryptedtext, BEncryptionPrivateKey)
+
+print receivedcleartext
+
+# Helpful example
+def encrypt(self, signatures):
+    # Encrypt the hash, using AES
+    # Receives signatures array
+    message = self.hash
+    for sign in signatures:
+        AEScipher = AESCipher(sign["hash"])
+        message = AEScipher.encrypt(message)
+    self.signature = message
+
+def decrypt(self):
+    # Returns the signature hash
+    message = self.signature
+    signatures = self.signatures.all()
+    for sign in signatures[::-1]:
+        AEScipher = AESCipher(sign.hash) # Accesing Signature property
+        message = AEScipher.decrypt(message)
+    return message
