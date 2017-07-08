c746e7f179f21936a533fa4aa7572751d2790bc3
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-07-08 09:55:36 -0500

    add detail views

--- prescryptchain/prescryptchain/views.py
+++ prescryptchain/prescryptchain/views.py
@@ -6,3 +6,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", {})
+
+
+def block_detail(request, block_hash):
+    return render(request, "blockchain/block_detail.html", {})
+
+def rx_detail(request, rx_hash):
+    return render(request, "blockchain/rx_detail.html", {})
