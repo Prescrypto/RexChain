c6129d540c7b136df29320babe0bf06680dec317
Everardo J. Barojas M <everardo@climb.mx>
2017-07-08 22:55:39 -0500

    Create and sign Rxs with simple timestamp

--- prescryptchain/blockchain/views.py
+++ prescryptchain/blockchain/views.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.views.generic import View, CreateView, ListView
 from django.shortcuts import render
 from blockchain.forms import NewPrescriptionForm
+from django.http import HttpResponseRedirect
+import hashlib
 
 # Create your views here.
 # Create new prescription view, this is the biz
@@ -19,5 +21,10 @@ class AddPrescriptionView(View):
         template = 'blockchain/new_rx.html'
         form = NewPrescriptionForm(request.POST)
         if form.is_valid():
-            print "yes"
-        return render(request, template ,{"data": prescription_data})
+            rx = form.save(commit = False)
+            rx.save()
+            hash_object = hashlib.sha256(str(rx.timestamp))
+            rx.signature = hash_object.hexdigest()
+            rx.save()
+
+        return HttpResponseRedirect('/')
--- prescryptchain/prescryptchain/urls.py
+++ prescryptchain/prescryptchain/urls.py
@@ -32,4 +32,4 @@ urlpatterns = [
 
 # Show images stored local in dev
 if settings.DEBUG:
-  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
