24fbdd0e763493d0839baad9be3eeff22a373964
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-07-11 16:25:35 -0500

    remove permissions to edit and add links to home

--- prescryptchain/blockchain/admin.py
+++ prescryptchain/blockchain/admin.py
@@ -7,9 +7,19 @@ from .models import Block, Prescription, Medication
 
 class PrescriptionAdmin(admin.ModelAdmin):
     ''' Custom Prescription Admin  '''
+    def has_add_permission(self, request, obj=None):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
     search_fields = ['id']
     list_per_page = 25
     fields = ('id','public_key', 'medic_name', 'patient_name','timestamp')
+    exclude = ('public_key','private_key',)
+    readonly_fields = ("public_key", "private_key", "medic_name",
+        "medic_cedula", "medic_hospital", "patient_name", "patient_age",
+        "diagnosis", "timestamp", "location", "details", "extras", "bought", "signature")
     # inlines = [MedicationInline, ]
 
 
