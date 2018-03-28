899fd1611d8bc3283d2734ebd7bddd84d71355af
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-03-28 15:13:53 -0600

    add safe set cache function

--- prescryptchain/core/helpers.py
+++ prescryptchain/core/helpers.py
@@ -2,10 +2,16 @@
 ''' Helper tools for rxchain '''
 import logging
 from django_rq import job
+from django.core.cache import cache
 
-
-@job
-def say_hello(name="World"):
+def safe_set_cache(key, value):
     ''' Just kind '''
     logger = logging.getLogger('django_info')
-    logger.info("Hello {}".format(name))
+    try:
+        cache.set(key, value)
+        cache.persist(key)
+        logger.info('[SUCCESS]Saving redis key:{}, value:{}'.format(key, value))
+    except Exception as e:
+        logger.error('[ERROR SET variables on Redis]: {}, type:{}'.format(e, type(e)))
+        raise e
+
