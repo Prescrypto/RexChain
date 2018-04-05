99e71dfd00a8fb3a578c5fb1ae5aa942aaafaab2
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-04-04 19:17:25 -0500

    Fix temporary data

--- prescryptchain/blockchain/models.py
+++ prescryptchain/blockchain/models.py
@@ -178,6 +178,10 @@ class PrescriptionManager(models.Manager):
         rx.medic_hospital = bin2hex(encrypt_with_public_key(data["medic_hospital"].encode("utf-8"), pub_key))
         rx.patient_name = bin2hex(encrypt_with_public_key(data["patient_name"].encode("utf-8"), pub_key))
         rx.patient_age = bin2hex(encrypt_with_public_key(str(data["patient_age"]).encode("utf-8"), pub_key))
+        # Temporary fix overflow problems
+        # TODO fix problem with rsa encrypts with too long characters
+        if len(data['diagnosis']) > 52:
+            data['diagnosis'] = data['diagnosis'][0:52]
         rx.diagnosis = bin2hex(encrypt_with_public_key(data["diagnosis"].encode("utf-8"), pub_key))
 
         # This is basically the address
