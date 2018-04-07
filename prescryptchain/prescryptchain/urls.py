56947a61c1cfacb0c11f51bf47d3d551f9a9a9cf
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-04-06 19:20:37 -0500

    fix static root on urls

--- prescryptchain/prescryptchain/urls.py
+++ prescryptchain/prescryptchain/urls.py
@@ -43,4 +43,4 @@ urlpatterns = [
 
 # Show images stored local in dev
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
