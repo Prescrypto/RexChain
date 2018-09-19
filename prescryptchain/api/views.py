d5cc07e21378f33a400ce93c491930521d14e85a
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-09-19 13:12:06 -0500

    add addresses endpoint

--- prescryptchain/api/views.py
+++ prescryptchain/api/views.py
@@ -14,10 +14,12 @@ from rest_framework import mixins, generics
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 # our models
-from blockchain.models import Block, Prescription, Medication, Transaction
+from blockchain.models import Block, Prescription, Medication, Transaction, Address
 from blockchain.utils import pubkey_string_to_rsa, pubkey_base64_to_rsa, pubkey_base64_from_uri
 
 from blockchain.helpers import CryptoTools
+
+from .exceptions import NonValidPubKey
 
 # Define router
 router = routers.DefaultRouter()
@@ -101,7 +103,7 @@ class PrescriptionViewSet(viewsets.ModelViewSet):
                 hex_raw_pub_key = _crypto.savify_key(pub_key)
             except Exception as e:
                 logger.error("[Public Key Error]:{}, type:{}".format(e, type(e)))
-                raise e
+                raise NonValidPubKey
 
             return Prescription.objects.filter(public_key=hex_raw_pub_key).order_by('-id')
         else:
@@ -140,3 +142,45 @@ class BlockViewSet(viewsets.ModelViewSet):
 
 # add patient filter by email, after could modify with other
 router.register(r'block', BlockViewSet, 'block-endpoint')
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    """ Address serializer """
+    pub_key = serializers.CharField(read_only=True,allow_null=True, source="get_pub_key" )
+
+    class Meta:
+        model = Address
+        fields = (
+            'public_key_b64',
+            'address',
+            'is_valid',
+            'pub_key',
+        )
+        read_only_fields = ('address','pub_key', )
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    """ Prescription Viewset """
+    serializer_class = AddressSerializer
+    lookup_field = "address"
+    http_method_names = ['get', 'options']
+
+    def get_queryset(self):
+        ''' Custom Get queryset '''
+        raw_public_key = self.request.query_params.get('public_key', None)
+        if raw_public_key:
+            try:
+                pub_key_b64 = pubkey_base64_from_uri(raw_public_key)
+
+            except Exception as e:
+                raise NonValidPubKey
+            else:
+                _address = Address.objects.get_or_create_rsa_address(pub_key_b64)
+                return Address.objects.filter(address=_address)
+
+        else:
+            return Address.objects.all()
+
+
+# add patient filter by email, after could modify with other
+router.register(r'address', AddressViewSet, 'address_endpoint')
