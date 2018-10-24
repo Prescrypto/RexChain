a8b3b359c79f2bc5f6eea623102dcca38d29cb93
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-10-24 14:23:22 -0500

    update permissions on admin

--- rexchain/blockchain/admin.py
+++ rexchain/blockchain/admin.py
@@ -2,27 +2,26 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Block, Prescription
+from .models import Block, Payload
 
 
-class PrescriptionAdmin(admin.ModelAdmin):
-    ''' Custom Prescription Admin  '''
+class PayloadAdmin(admin.ModelAdmin):
+    ''' Custom Payload Admin  '''
     def has_add_permission(self, request, obj=None):
         return True
 
     def has_delete_permission(self, request, obj=None):
         return False
 
+    def has_change_permission(self, request, obj=None):
+        return False
+
     search_fields = ['id']
     list_per_page = 25
-    fields = ('id','public_key', 'medic_name', 'patient_name','timestamp')
-    exclude = ('public_key','private_key',)
-    readonly_fields = ("public_key", "private_key", "medic_name",
-        "medic_cedula", "medic_hospital", "patient_name", "patient_age",
-        "diagnosis", "timestamp", "location", "details", "extras", "bought", "signature")
-    # inlines = [MedicationInline, ]
+    fields = ('id','public_key','timestamp',)
+    exclude = ('public_key', )
+    readonly_fields = ("public_key", "private_key", "data", "signature")
 
 
-# Register your models here.
 admin.site.register(Block)
-admin.site.register(Prescription)
+admin.site.register(Payload)
