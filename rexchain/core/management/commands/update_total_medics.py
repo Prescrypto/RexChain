e8b074a595cf3bfd694ddf92207af263c8bee212
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2018-10-30 15:29:22 -0600

    update comments

--- rexchain/core/management/commands/update_total_medics.py
+++ rexchain/core/management/commands/update_total_medics.py
@@ -7,7 +7,8 @@ from blockchain.models import Payload
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         ''' Update total medics on plataform
-        The reason is the queryset takes long time, better handle with cron thant realtime webserver request!
+        The reason is that the queryset takes long time,
+        better handle it with a cron than a realtime webserver request!
         '''
         total = Payload.objects.values("public_key").distinct("public_key").count()
         safe_set_cache("total_medics", total)
