0bfb6e7cc4893d5f592e1121f7c0db8b2ef2dfcf
Miguel Evangelista <miguel.eva.alv@gmail.com>
2019-04-29 17:50:51 -0500

    update rexchain/rexchain/forms file to meet pep8 style

--- rexchain/rexchain/forms.py
+++ rexchain/rexchain/forms.py
@@ -1,11 +1,12 @@
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
+
 import logging
 from django import forms
-
 from core.helpers import create_jira_issue
 
 logger = logging.getLogger('django_info')
+
 
 class AskCtaEmailForm(forms.Form):
     email = forms.EmailField(required=True)
@@ -19,5 +20,3 @@ class AskCtaEmailForm(forms.Form):
                 description='This user ask for more info in landingpage: {}'.format(email))
         except Exception as e:
             logger.error("[AskCTAEMAIL JIRA ERROR]: {}".format(e))
-
-
