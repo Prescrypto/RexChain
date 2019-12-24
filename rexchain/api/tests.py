8fdbe76c382aa48f80f4483912ebc748a209b9d5
Jesus Alvarado Garzon <xtornasol512@gmail.com>
2019-12-24 10:37:51 -0600

    fix whiet lines pep8

--- rexchain/api/tests.py
+++ rexchain/api/tests.py
@@ -7,20 +7,17 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 # from rest_framework.authtoken.models import Token
 
-
 logger = logging.getLogger('django_info')
 
 
 class RexchainBaseTestCase(TestCase):
     fixtures = ["initial_data.json"]
 
-
     def setUp(self):
         # Test definitions
         self.client = self._authenticate()
         self.content_type = 'application/json'
         self.format = 'json'
-
 
     def _authenticate(self):
         """ Perform API Client Here! """
@@ -39,4 +36,3 @@ class TestGetLastTransactions(RexchainBaseTestCase):
         logger.info("Starting Test on Block List")
         r = self.client.get('/api/v1/block/')
         self.assertEqual(r.status_code, 200)
-
