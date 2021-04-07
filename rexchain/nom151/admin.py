690ad3b96089be54d8a8dde3ff3e85bbe7f6a25d
Jesus Alvarado <xtornasol512@gmail.com>
2021-04-06 22:56:21 -0500

    fix flake8 warnings

--- rexchain/nom151/admin.py
+++ rexchain/nom151/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ConservationCertificate
+
+
+class ConservationCertificateAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ConservationCertificate, ConservationCertificateAdmin)
--- rexchain/nom151/models.py
+++ rexchain/nom151/models.py
@@ -6,30 +6,25 @@ from core.behaviors import Timestampable
 
 
 class ConservationCertificate(Timestampable, models.Model):
-	""" This metadata is received by ReachCore identity as an official document for PoE """
+    """ This metadata is received by ReachCore identity as an official document for PoE """
 
-	# Review if neccesary block relationship
-	# block = models.OneToOneField('blockchain.Block', related_name='certificate', null=True, blank=True
+    # Review if neccesary block relationship
+    # block = models.OneToOneField('blockchain.Block', related_name='certificate', null=True, blank=True
 
-	# Regular fields
-	folio = models.CharField("Certificate Folio",
-			max_length=255, blank=True, help_text='External Identifier')
-	raw_document = models.TextField("Raw Certificate",
-			blank=True, help_text='Base64 document')
-	reference = models.CharField("Merkle Root used as reference",
-			max_length=255, help_text='This ir our reference')
+    # Regular fields
+    folio = models.CharField("Certificate Folio",
+                             max_length=255, blank=True, help_text='External Identifier')
+    raw_document = models.TextField("Raw Certificate",
+                                    blank=True, help_text='Base64 document')
+    reference = models.CharField("Merkle Root used as reference",
+                                 max_length=255, help_text='This ir our reference')
 
-	# Metadata
-	data = JSONField(default={}, blank=True)
+    # Metadata
+    data = JSONField(default={}, blank=True)
 
-
-	def __str__(self):
-		""" Return custom string for the object
-		raw_document first 6 digits then merkle_root first 6 digits too
-		"""
-		SLICE = 6
-		return F"{self.raw_document[:SLICE]} - {self.reference[:6]}"
-
-
-
-
+    def __str__(self):
+        """ Return custom string for the object
+        raw_document first 6 digits then merkle_root first 6 digits too
+        """
+        SLICE = 6
+        return F"{self.raw_document[:SLICE]} - {self.reference[:6]}"
