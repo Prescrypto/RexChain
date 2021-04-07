f7a89487d2355f62daba924e190f46579fb06725
Jesus Alvarado <xtornasol512@gmail.com>
2021-04-06 21:52:26 -0500

    add nom151 namespace app

---
+++ rexchain/nom151/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Nom151Config(AppConfig):
+    name = 'nom151'
--- rexchain/rexchain/settings.py
+++ rexchain/rexchain/settings.py
@@ -85,6 +85,7 @@ INSTALLED_APPS = [
     'api',
     'blockchain',
     'core',
+    'nom151',
     # CORS
     'corsheaders',
 ]
