2defbf28b9a0ac22426e23ba2fae4a33c0d4a880
Everardo J Barojas M <everardo@prescrypto.com>
2024-10-17 00:11:13 -0600

    Fix syntax

--- rexchain/rexchain/settings.py
+++ rexchain/rexchain/settings.py
@@ -202,7 +202,7 @@ REDIS_URL = os.getenv("REDIS_URL", 'redis://localhost:6379/0')
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": REDIS_URL # os.getenv('REDIS_TEMPORARY_URL', 'redis://localhost:6379/0'),
+        "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "CONNECTION_POOL_KWARGS": {
